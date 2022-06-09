5d16a34ca931dfba784c69bab75da029984f5ecb
alantothe <rubenamalpartida@gmail.com>
2022-06-08 21:09:01 -0400

    add js file

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ console.log("Challenge 1");
 // Challenge 1
 //
 // Loop through the following array, console.logging out each value.
-const students = [
+let students = [
     'Tre',
     'Sonny',
     'Crystal',
@@ -26,14 +26,24 @@ const students = [
   ];
   // Challenge 1 Code
 
+  for (i=0 ; i <12 ;i++){
+    console.log(students[i])
+  }
+
 
 
   console.log("\n");
   console.log("Challenge 2");
   // Challenge 2
-  // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
+  // Loop through the following array BACKWARDS, console.logging out each value. 64 
+  //should be printed first and 100 last.
   const grades = [100, 80, 110, 75, 83, 64];
   //Challenge 2 Code
+
+  for (i= grades.length-1; i>= 0 ;i--){
+    console.log(grades[i])
+  }
+
 
   
 
@@ -44,14 +54,29 @@ const students = [
   // Console.log out only the even numbers in the following array.
   const positiveNumbers = [5, 2, 13, 17, 4, 102, 3000];
   // Challenge 3 Code
+positiveNumbers.shift()
+positiveNumbers.splice(1, 2)
+
+  console.log(positiveNumbers)
+
+ 
+
+
 
 
 
   console.log("\n");
   console.log("Challenge 4");
   // Challenge 4
-  // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
+  // Console.log out the even numbers in the following array, INCLUDING the negative ones. 
+  // There should be four total printings.
   const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
+mixedSignNumbers.shift()
+mixedSignNumbers.shift()
+mixedSignNumbers.splice(2, 1)
+mixedSignNumbers.splice(3, 1)
+
+console.log(mixedSignNumbers)  
 
   // Challenge 4 Code
 
@@ -61,8 +86,19 @@ const students = [
   console.log("\n");
   console.log("Challenge 5");
   // Challenge 5
-  // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
+  // Remove two values from the beginning and one value from the end of the following array,
+  //  simply by deleting them in the following line. Then console.log out each value individually.
   const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
+
+  symmetricalCapitals.shift()
+  symmetricalCapitals.shift()
+  symmetricalCapitals.pop()
+
+  for (i=0; i<= symmetricalCapitals.length;i++){
+    console.log(symmetricalCapitals[i])
+  }
+
+
   // Challenge 5 Code
     
 
@@ -70,19 +106,37 @@ const students = [
   console.log("\n");
   console.log("Challenge 6");
   // Challenge 6
-  // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
+  // Add a value (your choice!) to the start of the following array and two values (of your choice)
+  // to the end simply by adding them in the following line, and then console.log out each 
+  // value individually.
   const fibonacciNumbers = [1, 1, 2, 3, 5, 8, 13];
+
   // Challenge 6 Code
-  
+  fibonacciNumbers.push(21)
+
+  for (i=0;i<=fibonacciNumbers.length-1; i++){
+    console.log(fibonacciNumbers[i])
+  }  
   
 
 
   console.log("\n");
   console.log("Challenge 7");
   // Challenge 7
-  // Make a NEW array with 5 values of your choice, using the same syntax as the array creation from the previous 6 challenges; that is, every value should be placed within the square brackets.  Now console.log out each value individually.
+  // Make a NEW array with 5 values of your choice, using the same syntax as the array creation 
+  //from the previous 6 challenges; that is, every value should be placed within the square brackets.  
+  //Now console.log out each value individually.
+  const newArray = [1, 2, 3, 4, 5];
   // Challenge 7 Code
-
+  const alanArray = [
+    "byun",
+    "maru",
+    "clem",
+    "heromarine",
+    "gumiho",
+]
+for (i=0 ; i <=alanArray.length-1 ;i++)
+console.log(alanArray[i])
 
 
 
@@ -90,9 +144,19 @@ const students = [
   console.log("\n");
   console.log("Challenge 8");
   // Challenge 8
-  // Make a new EMPTY array and fill it with five values of your choice using BOTH .push AND .unshift, and then console.log out each value individually.
+  // Make a new EMPTY array and fill it with five values of your choice using BOTH .push AND .unshift, and 
+  //then console.log out each value individually.
   const emptyArray = [];
   // Challenge 8 Code
+
+  emptyArray.unshift("byun")
+  emptyArray.unshift("maru")
+  emptyArray.unshift("clem")
+  emptyArray.push("heromarine")
+  emptyArray.push("gumiho")
+
+  for (i=0 ; i <=4 ;i++)
+  console.log(emptyArray[i])
 
 
 
@@ -100,16 +164,51 @@ const students = [
   console.log("\n");
   console.log("Challenge 9");
   // Challenge 9
-  // Loop through the `students` array from Challenge 1, starting at index 3 and ending at index 10 (INCLUSIVE), logging out each value individually.
+  // Loop through the `students` array from Challenge 1, starting 
+  //at index 3 and ending at index 10 (INCLUSIVE), 
+  //logging out each value individually.
   // Challenge 9 Code
   
+
+
+   students.splice(0,3);
+   
+  
+
+
+ for(i=0; i<=6;i++){
+console.log(students[i])
+ }
+
+
+
   
   console.log("\n");
   console.log("Challenge 10");
+  
   // Challenge 10
-  // Loop through the `students` array from Challenge 1, making a COPY of the array , starting at index 3 and ending at index 10 (INCLUSIVE). This is very similar to string building; create a new array, loop through the old one, and put values from the original in the new one. Then log out each value of the copy indivudally.
+  // Loop through the `students` array from Challenge 1, making a COPY of the array , starting at 
+  //index 3 and ending at index 10 (INCLUSIVE). 
+  //This is very similar 
+  //to string building; create a new array, loop through the old one, and put values from the original 
+  //in the new one. Then log out each value of the copy indivudally.
   // Do NOT use slice, you will be using that for the next challenge!
   // Challenge 10 Code
+ 
+students.splice(0,4) 
+for(i=0; i<=students.length-1;i++){
+console.log(students[i])
+}
+fillArray = []
+
+fillArray.push(students)
+
+
+for(i=0; i<=fillArray.length-1;i++){
+  console.log(fillArray[i])
+}
+
+
 
 
 
@@ -119,25 +218,52 @@ const students = [
   // Challenge 11
   // Make a COPY of the `students` array *using .slice*, starting at index 3 and ending at index 10 (INCLUSIVE), then log out each value of the copy individually. THEN log out each value of the original array. Notice that the original array didn't get any values taken out of it, because `slice` does NOT modify the original array.
   // Challenge 11 Code
+  students.slice(3, 11)
   
+  for(i=0; i<=students.length-1;i++){
+    console.log(students[i])
+  }
 
 
 
   console.log("\n");
   console.log("Challenge 12");
   // Challenge 12
-  // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array. Note that .splice DOES change the original array.
-  const dinosaurs = ['Velociraptor', 'T-Rex', 'Stegosaurus', 'Triceratops', 'Dimetrodon', 'Allosaur', 'Spinosaurus', 'Gigantosaur']
+  // Pull the items at indices 4-6 (inclusive) from the following array using .splice (NOT .slice!), then console.log out each item from the ORIGINAL array.
+  // Note that .splice DOES change the original array.
+  const dinosaurs = [
+    'Velociraptor', 
+    'T-Rex', 
+    'Stegosaurus', 
+    'Triceratops', 
+    'Dimetrodon', 
+    'Allosaur', 
+    'Spinosaurus', 
+    'Gigantosaur'
+  ]
   // Challenge 12 Code
+dinosaurs.splice(4, 3)
 
+
+
+for(i=0;i<=dinosaurs.length-1; i++)
+console.log(dinosaurs[i])
   
   
   
   console.log("\n");
   console.log("Challenge 13");
   // Challenge 13
-  // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the "separator". Then console.log out that string.
+  // Use .join to change the dinosaur strings from the above array into one string with a '*' string as the 
+  //"separator". Then console.log out that string.
   // Challenge 13 Code
+
+ 
+  console.log(dinosaurs.join('*'))
+
+  
+
+
 
 
 
@@ -146,8 +272,13 @@ const students = [
 
   console.log("Challenge 14");
   // Challenge 14
-  // Use .reverse to reverse the dinosaur array and then log out the original array. .reverse DOES change the original array.
+  // Use .reverse to reverse the dinosaur array and then log out the original array. .
+  //reverse DOES change the original array.
   // Challenge 14 Code
+  dinosaurs.reverse()
+  
+  console.log(dinosaurs)
+ 
 
 
 
@@ -155,9 +286,29 @@ const students = [
   console.log("\n");
   console.log("Challenge 15");
   // Challenge 15
-  // Create two new arrays.
-  // Use .concat to combine those two arrays into a NEW array you make (.concat RETURNS an array). Then console.log out each value of that new array individually. Now console.log both the primaries and secondaries arrays, noting that .concat does NOT mutate the original arrays.
+  // Use .concat to combine the following two arrays into a NEW array you make (.concat RETURNS an array).
+  // Then console.log out each value of that new array individually. Now console.log both the primaries 
+  //and secondaries arrays, noting that .concat does NOT mutate the original arrays.
   // Challenge 15 Code
+
+  allElements = dinosaurs.concat(students)
+
+  for (i=0;i <= allElements.length-1;i++){
+    console.log(allElements[i])
+
+  }
+  console.log("\n");
+
+  for(i=0;i<=dinosaurs.length-1; i++)
+console.log(dinosaurs[i])
+
+console.log("\n");
+
+ for(i=0;i<=students.length-1; i++)
+console.log(students[i])
+
+  
+  
 
 
   
